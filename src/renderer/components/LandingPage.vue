63ccfc350886d674d3dbc23824300de777e39005
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2018-09-13 13:58:18 -0400

    Added non-working vue-file-explorer

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,43 +1,197 @@
 <template>
   <div id="wrapper">
-    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
     <main>
-      <div class="left-side">
-        <span class="title">
-          Welcome to your new project!
+      <div>
+          This project uses
+          <a href="https://npmjs.com/package/@gurupras/vue-file-explorer" target="_blank">@gurupras/vue-file-explorer</a>
+          which internally uses
+          <a href="https://npmjs.com/package/@gurupras/file-icons-js" target="_blank">@gurupras/file-icons-js</a>.
+          <p>
+            The <code>@gurupras/file-icons-js</code> library imports a few <code>woff2</code> files in its CSS.<br>
+            These files fail to be imported properly in this electron project.
+          </p>
         </span>
-        <system-information></system-information>
       </div>
 
-      <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
-        </div>
+      <div style="margin-top: 5em;">
+        <vue-file-explorer v-for="mount in mountPoints" :key="mount.name"
+                :absolute-path="mount.absolutePath"
+                :is-directory="mount.isDirectory"
+                :name="mount.name"
+                :children="mount.children"
+                @selected="onSelect"
+                @update-directory="onUpdateDirectory"/>
       </div>
     </main>
   </div>
 </template>
 
 <script>
-  import SystemInformation from './LandingPage/SystemInformation'
+  import VueFileExplorer from '@gurupras/vue-file-explorer'
 
   export default {
     name: 'landing-page',
-    components: { SystemInformation },
+    components: { VueFileExplorer },
+    data () {
+      return {
+        selected: [],
+        mountPoints: [
+          {
+            name: '/home',
+            isDirectory: true,
+            children: [
+              {
+                name: 'Downloads',
+                isDirectory: true,
+                children: [
+                  {
+                    name: 'icon-pack.zip',
+                    isDirectory: false
+                  },
+                  {
+                    name: 'README.md',
+                    isDirectory: false
+                  },
+                  {
+                    name: 'form-37742.docx',
+                    isDirectory: false
+                  }
+                ]
+              },
+              {
+                isDirectory: true,
+                name: 'Workspace',
+                children: [
+                  {
+                    name: 'file-browser',
+                    isDirectory: true,
+                    children: [
+                      {
+                        name: 'src',
+                        isDirectory: true,
+                        children: [
+                          {
+                            name: 'assets',
+                            isDirectory: true,
+                            children: [
+                              {
+                                name: 'favicon.ico',
+                                isDirectory: false
+                              }
+                            ]
+                          },
+                          {
+                            name: 'components',
+                            isDirectory: true,
+                            children: [
+                              {
+                                name: 'entry.vue',
+                                isDirectory: false
+                              }
+                            ]
+                          },
+                          {
+                            name: 'views',
+                            isDirectory: true,
+                            children: []
+                          },
+                          {
+                            name: 'App.vue',
+                            isDirectory: false
+                          },
+                          {
+                            name: 'main.js',
+                            isDirectory: false
+                          },
+                          {
+                            name: 'router.js',
+                            isDirectory: false
+                          },
+                          {
+                            name: 'store.js',
+                            isDirectory: false
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              },
+              {
+                absolutePath: `D:\\Videos\\2016\\vacation.mp4`,
+                isDirectory: false,
+                name: 'vacation.mp4'
+              }
+            ]
+          },
+          {
+            name: '/usr',
+            isDirectory: true,
+            children: [
+              {
+                name: 'lib',
+                isDirectory: true,
+                children: [
+                  {
+                    name: 'libau.so',
+                    isDirectory: false
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    },
     methods: {
       open (link) {
         this.$electron.shell.openExternal(link)
+      },
+      clearAllSelected () {
+        this.selected.forEach(entry => (entry.dataset.selected = 'false'))
+        this.selected = []
+      },
+      onSelect ({ event, paths, component }) {
+        var ignoreSelect = false
+        var entries = [event.target.closest('.entry')]
+
+        if (event.shiftSelect) {
+        // Ensure that this element and other selected elements have the same parent
+        // If not, ignore this click
+        // Since we do this for every multi-select, it is sufficient to just check
+        // the parent of the 0th element
+          const entry = entries[0]
+          const parent = entry.parentNode.closest('.entry')
+          // Find all contiguous entries between the last selected entry and the currently selected entry
+          // First, get the top-most selected entry
+          const lastSelectedEntry = this.selected.splice(-1)[0]
+          try {
+            const children = parent.children
+            const childrenArray = Array.from(children)
+            const lastSelectedEntryIdx = childrenArray.indexOf(lastSelectedEntry.parentNode)
+            const currentIndex = childrenArray.indexOf(entry.parentNode)
+            const minIdx = Math.min(lastSelectedEntryIdx, currentIndex)
+            const maxIdx = Math.max(lastSelectedEntryIdx, currentIndex)
+            entries = childrenArray.map((entry, idx) => idx >= minIdx && idx <= maxIdx ? entry.querySelector('.entry') : undefined).filter(x => !!x)
+          } catch (e) {
+          }
+        }
+
+        if (!event.multiSelect) {
+        // Clear out the selected array
+          this.clearAllSelected()
+        }
+
+        if (ignoreSelect) {
+          return
+        }
+        entries.forEach(entry => {
+          entry.dataset.selected = 'true'
+          this.selected.push(entry)
+        }, this)
+      },
+      onUpdateDirectory (data) {
+
       }
     }
   }
@@ -72,57 +226,8 @@
     width: 420px;
   }
 
-  main {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  main > div { flex-basis: 50%; }
-
-  .left-side {
-    display: flex;
-    flex-direction: column;
-  }
-
-  .welcome {
-    color: #555;
-    font-size: 23px;
-    margin-bottom: 10px;
-  }
-
-  .title {
-    color: #2c3e50;
-    font-size: 20px;
-    font-weight: bold;
-    margin-bottom: 6px;
-  }
-
-  .title.alt {
-    font-size: 18px;
-    margin-bottom: 10px;
-  }
-
-  .doc p {
-    color: black;
-    margin-bottom: 10px;
-  }
-
-  .doc button {
-    font-size: .8em;
-    cursor: pointer;
-    outline: none;
-    padding: 0.75em 2em;
-    border-radius: 2em;
-    display: inline-block;
-    color: #fff;
-    background-color: #4fc08d;
-    transition: all 0.15s ease;
-    box-sizing: border-box;
-    border: 1px solid #4fc08d;
-  }
-
-  .doc button.alt {
-    color: #42b983;
-    background-color: transparent;
+  .center {
+    margin-left: 50%;
+    margin-right: 50%;
   }
 </style>
